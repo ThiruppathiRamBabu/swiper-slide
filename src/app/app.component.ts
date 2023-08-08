3360cd4846f4436fe85cccbedfe30bdfceef6866
ThiruppathiRamBabu <snthiru2000@gmail.com>
2023-08-08 14:32:29 +0530

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {register} from 'swiper/element/bundle';
+import { register } from 'swiper/element/bundle';
 register();
 
 @Component({
@@ -8,5 +8,5 @@ register();
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor() { }
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,12 +7,28 @@ import Swiper from 'swiper';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-images = [
-  '../assets/david-svihovec-Gd6hKygUGi8-unsplash.jpg',
-  '../assets/rami-al-zayat-w33-zg-dNL4-unsplash.jpg',
-  '../assets/robin-worrall-FPt10LXK0cg-unsplash.jpg',
-  '../assets/rodion-kutsaiev-0VGG7cqTwCo-unsplash.jpg',
-];  
-  constructor() {}
+  @ViewChild('swiper')
+  swiperRef: ElementRef | undefined;
+  swiper?: Swiper;
 
+  images = [
+    'https://images.unsplash.com/photo-1483478550801-ceba5fe50e8e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8bW9iaWxlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1480694313141-fce5e697ee25?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fG1vYmlsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1528795259021-d8c86e14354c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fG1vYmlsZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60',
+    'https://images.unsplash.com/photo-1532356884227-66d7c0e9e4c2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bW9iaWxlfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60',
+  ];
+  constructor() { }
+
+  swiperReady() {
+    this.swiper = this.swiperRef?.nativeElement.swiper;
+  }
+  swiperSlideChanged(e: any) {
+    console.log('changed: ', e);
+  }
+  goNext() {
+    this.swiper?.slideNext(1);
+  }
+  goPrev() {
+    this.swiper?.slidePrev(1);  
+  }
 }
